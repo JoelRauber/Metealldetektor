be389871e4c95ca9907685661cca0b69335370ea
M-S\yaz <yzuellig@gmail.com>
2018-09-26 14:59:05 +0200

    ADDED: MAGNET Sensor

--- app/src/main/java/com/example/jor/metealldetektor/MainActivity.java
+++ app/src/main/java/com/example/jor/metealldetektor/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.jor.metealldetektor;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,10 +10,10 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.TextureView;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
@@ -66,6 +68,51 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        //not in use
+
+    }
+
+    private static final int SCAN_QR_CODE_REQUEST_CODE = 0;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem menuItem = menu.add("Log");
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                startActivityForResult(intent, SCAN_QR_CODE_REQUEST_CODE);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == SCAN_QR_CODE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                String logMsg = intent.getStringExtra("SCAN_RESULT");
+
+                log(logMsg);
+            }
+        }
+    }
+
+    private void log(String qrCode) {
+        Intent intent = new Intent("ch.appquest.intent.LOG");
+
+        if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty()) {
+            Toast.makeText(this, "Logbook App not Installed", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Achtung, je nach App wird etwas anderes eingetragen
+        String logmessage = "{\n" + "  \"task\": \"Metalldetektor\",\n" + "  \"solution\": \" "+ qrCode+" \"\n" +"}";
+        intent.putExtra("ch.appquest.logmessage", logmessage);
+
+        startActivity(intent);
     }
 }
